8dc1a58b354a0b55847fcc16ce76c8bf110b5f4f
Levin <Gil.Levin@emc.com>
2019-08-06 00:22:51 +0300

    added ssh-copy-id fail note in exercise5

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -4,3 +4,6 @@ ssh-keygen -f ~/.ssh/id_rsa -P ""
 ssh-copy-id server2
 #could have installed expect and wait for the 
 #password prompt to populate it with the users pass
+
+
+##NOTE: ssh-copy-id will fail the "vagrant up" command since server2 isn't up yet.
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -4,3 +4,6 @@ ssh-keygen -f ~/.ssh/id_rsa -P ""
 ssh-copy-id server1
 #could have installed expect and wait for the 
 #password prompt to populate it with the users pass
+
+
+##NOTE: ssh-copy-id will fail the "vagrant up" command since server1 isn't up yet.
