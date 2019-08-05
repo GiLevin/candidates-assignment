b856c1d41a4fb7d9e22ca4c724318ca26f76fc11
Levin <Gil.Levin@emc.com>
2019-08-05 23:53:34 +0300

    added fixScript. - Using vagrant the VM's won't be provishioned parallely, so coping the ssh-key will fail since server2 is down. - could also avoid the prompt in "ssh-copy-id" if installed "expect" and wainted for relavnt prompt.

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -f ~/.ssh/id_rsa -P ""
+ssh-copy-id server2
+#could have installed expect and wait for the 
+#password prompt to populate it with the users pass
